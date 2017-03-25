d693d6bbe24e779dae36c77a3a3bbb1db70d165a
TristinH <tristin95@yahoo.com>
2017-03-24 19:07:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
                                autoescape=True)
 
-s = 'ko12je,2o4908ifeop,kl2kofwh893hr09op32mrl'
+# add secret here
+s = ''
 
 
 # secure a cookie
